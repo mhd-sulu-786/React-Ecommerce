98decf680f1b5ba7f0ed56081057b9a84a43a403
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-06-06 13:10:38 +0530

    Update Home.jsx

--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
         <div className='hero' id='action1'>
             <Card className="bg-dark text-white border-0  " id='action1'>
-<Card.Img src=" https://images.app.goo.gl/FsAgxZNtcrjhUpMz7" alt="bg-shoe" height={'550px'} />
+<Card.Img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuiIP115O3WtT4iV59Zl2D9-XnZnzD42K6EA&usqp=CAU" alt="bg-shoe" height={'550px'} />
                 <Card.ImgOverlay className='card-img-overplay  d-flex flex-column justify-content-center '>
                     <Card.Title className='card-title fw-bold mb-0 fs-1 display-3 px-3'>NEW SEASON ARRIVALS</Card.Title>
                     <Card.Text className='card-text lead fs-2 p-2'>CHECK OUT ALL THE TRENDS</Card.Text>
