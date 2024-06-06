08e3079eb416c735282b462fc57337791ba221d3
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-06-06 12:39:41 +0530

    Update Home.jsx

--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
         <div className='hero' id='action1'>
             <Card className="bg-dark text-white border-0  " id='action1'>
-                <Card.Img src="https://www.hdwallpapers.in/download/red_nike_shoe_in_black_background_4k_hd_nike-HD.jpg" alt="bg-shoe" height={'550px'} />
+<Card.Img src=" https://media.istockphoto.com/photos/nurse-welcomes-senior-couple-in-hospital-picture-id471870047?k=20&m=471870047&s=612x612&w=0&h=cJwV1g6GHX5y6AvwozW9L0ngG-F70B_4PZCVleG8MrA=" alt="bg-shoe" height={'550px'} />
                 <Card.ImgOverlay className='card-img-overplay  d-flex flex-column justify-content-center '>
                     <Card.Title className='card-title fw-bold mb-0 fs-1 display-3 px-3'>NEW SEASON ARRIVALS</Card.Title>
                     <Card.Text className='card-text lead fs-2 p-2'>CHECK OUT ALL THE TRENDS</Card.Text>
